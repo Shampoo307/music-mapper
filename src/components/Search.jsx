d6db717d4ffdb52c983a566c5caa3cacb5a9e280
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-09-08 19:11:02 +1000

    base

---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import  React, { useState, useEffect } from 'react';
+
+
+
+export default function Search(props) {
+    return (
+        <main className="form">
+            <div>
+                <p>home</p>
+            </div>
+        </main>
+    );
+}
