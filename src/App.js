99ed8b434efeee56dc6a16128709d27522ff49ce
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-09-08 19:10:40 +1000

    router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
+import  React, { useState, useEffect } from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+
+import Form from "./pages/Form";
+import Map from "./pages/Map";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="App">
+            <Switch>
+                <Route exact path="/">
+                    <Form/>
+                </Route>
+				<Route exact path="/map">
+                    <Map/>
+                </Route>
+            </Switch>            
+        </div>
+    </Router>
   );
 }
 
