88cb5d2fcfbcc2a139b91ca8be675ec5b9bfc499
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-09-09 13:38:47 +1000

    call genre search api

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,13 +1,37 @@
 import  React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import Search from '../components/Search.jsx';
+import SearchResults from '../components/SearchResults.jsx';
+
+
+// search genres & countries with most artists of that genre
+// search artists and reveal artists similar to that one and their location 
+
 
 
 export default function Form(props) {
+    const [ genres, setGenres ] = useState([]);
+
+    const searchGenre = (genre) => {
+        fetch(`/api/genre/${genre}`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                setGenres(res);
+            });
+    }
+
     return (
         <main className="form">
             <div>
-                <p>home</p>
+                <h1>Music Mapper</h1>
+                <p>Welcome to Music Mapper!</p>
+                <p>Select up to three genres and see the regions with the most popular from each genre</p>
+                <p>or</p>
+                <p>Search for an artist and see the regions with the most artists similar to the one you searched for</p>
+                <Search handleSearch={searchGenre} />
+                { genres && <SearchResults itemList={genres}/>}
                 <Link to="/map">Map</Link>
             </div>
         </main>
