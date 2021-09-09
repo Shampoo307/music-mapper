8571aa60713688b44f25332b6e591e5d551ad80d
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-09-09 18:58:09 +1000

    lift state into parent component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,45 +1,33 @@
 import  React, { useState, useEffect } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
 
 
 export default function Search(props) {
-    
-    const [search, setSearch] = useState('');
-    const [error, setError] = useState('');
-
-    const handleChange = (event) => {
-        setSearch(event.target.value);
-    };
-    
-    const handleSearch = (event) => {
-        event.preventDefault();
-        if (search.trim() !== '') {
-            props.handleSearch(search);
-        } else {
-            setError('Please enter a valid search');
-        }
-    };
-
-
     return (
         <div>
-            <Form onSubmit={handleSearch}>
-                { error && <p className="errorMsg">{error}</p>}
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Enter search term</Form.Label>
-                    <Form.Control
-                        type="search"
-                        name="searchTerm"
-                        value={search}
-                        placeholder="Search for a genre"
-                        onChange={handleChange}
-                        autoComplete="off"
-                    />
+            <Form onSubmit={props.handleSearch}>
+                { props.error && <p className="errorMsg">{props.error}</p>}
+                <Form.Group>
+                    <Form.Label>Search for a genre</Form.Label>
+                    <Row>
+                        <Col>
+                            <Form.Control
+                                type="search"
+                                name="searchTerm"
+                                value={props.search}
+                                placeholder="Search for a genre"
+                                onChange={props.handleChange}
+                                autoComplete="off"
+                            />
+                        </Col>
+                        <Col xs="auto">
+                            <Button variant="info" type="submit">
+                                Search
+                            </Button>
+                        </Col>
+                    </Row>
                 </Form.Group>
-                <Button variant="info" type="submit">
-                    Search
-                </Button>
             </Form>
         </div>
     );
