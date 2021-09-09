8cc3e363e09eee4425d714ca590384e46319e134
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-09-09 18:58:36 +1000

    select search result functionality

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,12 +1,17 @@
 import  React, { useState, useEffect } from 'react';
-import { ListGroup } from 'react-bootstrap';
+import { ListGroup, Button } from 'react-bootstrap';
 
 
 
 export default function  SearchResults(props) {
-
     const items = props.itemList.map(element => {
-        return <ListGroup.Item>{element}</ListGroup.Item>
+        return (
+            <ListGroup.Item>
+                <Button onClick={props.onClick}>
+                    {element}
+                </Button>
+            </ListGroup.Item>
+        );
     });
 
     return (
