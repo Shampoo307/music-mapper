5749b0db200ba4ad6cf42b765b086e9735cca615
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-09-09 18:59:36 +1000

    abstract more to separate component

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,8 +1,9 @@
 import  React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
-import Search from '../components/Search.jsx';
-import SearchResults from '../components/SearchResults.jsx';
+import Genres from './Genres.jsx';
+import Artists from './Artists.jsx';
 
 
 // search genres & countries with most artists of that genre
@@ -11,16 +12,7 @@ import SearchResults from '../components/SearchResults.jsx';
 
 
 export default function Form(props) {
-    const [ genres, setGenres ] = useState([]);
-
-    const searchGenre = (genre) => {
-        fetch(`/api/genre/${genre}`)
-            .then(res => res.json())
-            .then(res => {
-                console.log(res);
-                setGenres(res);
-            });
-    }
+    const [ view, setView ] = useState('genres');
 
     return (
         <main className="form">
@@ -30,8 +22,14 @@ export default function Form(props) {
                 <p>Select up to three genres and see the regions with the most popular from each genre</p>
                 <p>or</p>
                 <p>Search for an artist and see the regions with the most artists similar to the one you searched for</p>
-                <Search handleSearch={searchGenre} />
-                { genres && <SearchResults itemList={genres}/>}
+                
+                <h2>Search by Genres</h2>
+                <h2>Search by Artists</h2>
+                <Container>
+                { view === 'genres' && <Genres /> }
+                { view === 'artists' && <Artists /> }
+                </Container>
+
                 <Link to="/map">Map</Link>
             </div>
         </main>
