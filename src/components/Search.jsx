847f5639b5bcb3bd7c3b6bf3752250bb02a8cb04
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-09-09 13:38:20 +1000

    make search bar & button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,46 @@
 import  React, { useState, useEffect } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 
 
 export default function Search(props) {
+    
+    const [search, setSearch] = useState('');
+    const [error, setError] = useState('');
+
+    const handleChange = (event) => {
+        setSearch(event.target.value);
+    };
+    
+    const handleSearch = (event) => {
+        event.preventDefault();
+        if (search.trim() !== '') {
+            props.handleSearch(search);
+        } else {
+            setError('Please enter a valid search');
+        }
+    };
+
+
     return (
-        <main className="form">
-            <div>
-                <p>home</p>
-            </div>
-        </main>
+        <div>
+            <Form onSubmit={handleSearch}>
+                { error && <p className="errorMsg">{error}</p>}
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Enter search term</Form.Label>
+                    <Form.Control
+                        type="search"
+                        name="searchTerm"
+                        value={search}
+                        placeholder="Search for a genre"
+                        onChange={handleChange}
+                        autoComplete="off"
+                    />
+                </Form.Group>
+                <Button variant="info" type="submit">
+                    Search
+                </Button>
+            </Form>
+        </div>
     );
 }
