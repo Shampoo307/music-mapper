5f5da163e1ecbbcc5b6076431b1d107f669d1ff9
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-09-18 17:40:04 +1000

    make artist form, display search results

--- src/pages/Artists.jsx
+++ src/pages/Artists.jsx
@@ -1,30 +1,103 @@
 import  React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col, Alert, Button } from 'react-bootstrap';
 
 import Search from '../components/Search.jsx';
 import SearchResults from '../components/SearchResults.jsx';
+import Selected from '../components/Selected.jsx';
 
 
 export default function Artists(props) {
-    const [ artists, setArtists ] = useState([]);
+    const [ artistsList, setArtists ] = useState([]);
+    const [ selectedArtist, setSelectedArtist ] = useState('');
+    const [ error, setError ] = useState('');
 
-    const searchGenre = (genre) => {
-        fetch(`/api/genre/${genre}`)
+    const [ artistSearch, setArtistSearch ] = useState('');
+    const [ searchError, setSearchError ] = useState('');
+
+    const [ results, setResults ] = useState('');
+
+    const searchArtist = (artist) => {
+        fetch(`/api/artist/${artist}`)
             .then(res => res.json())
             .then(res => {
-                console.log(res);
-                setArtists(res);
+                console.log('res from artist search', res);
+                // setArtists(res);
             });
     }
 
+    const handleSearchChange = (event) => {
+        setArtistSearch(event.target.value);
+    };
+    
+    const handleSearch = (event) => {
+        event.preventDefault();
+        if (searchError) {
+            setError('');
+        }
+        if (artistSearch.trim() !== '') {
+            searchArtist(artistSearch);
+        } else {
+            setSearchError('Please enter a valid search!');
+        }
+    }
+
+    const makeSelection = (event) => {
+        event.preventDefault();
+        if (selectedArtist !== '') {
+            setError('Unable to select more than artist!');
+        } else {
+            setSelectedArtist(null);
+            setArtists('');
+            setArtistSearch('');
+        }
+    };
+
+    const removeSelection = (event) => {
+        setSelectedArtist('');
+    };
+
     return (
-        <main className="form">
-            <div>
-                <Search handleSearch={searchGenre} />
-                { artists && <SearchResults itemList={artists}/>}
-                <Link to="/map">Map</Link>
-            </div>
-        </main>
+        <Container>
+            <Row>
+                <Col>
+                    <Row className="selections">
+                        {selectedArtist !== '' && 
+                            <Col className="selected-genres">
+                                <Button variant="outline-dark" onClick={removeSelection}>
+                                    {selectedArtist}
+                                </Button>
+                            </Col>
+                        }
+                    </Row>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    { error && <Alert variant="danger">{error}</Alert>}
+                    <Search 
+                        handleChange={handleSearchChange}
+                        handleSearch={handleSearch}
+                        searchText="Search for an artist"
+                        search={artistSearch}
+                        error={searchError}
+                    />
+                    { artistsList && <SearchResults itemList={artistsList} onClick={makeSelection} /> }
+                </Col>
+            </Row>
+            {/* <Button onClick={showOnMap}>
+                Display results!
+            </Button> */}
+            {/* <Link to={{ pathname: '/map', search: stringify({genres: {
+                    genre1: selectedGenres[0],
+                    genre2: selectedGenres[1],
+                    genre3: selectedGenres[2]
+                }}) 
+            }}>Show results on a map!</Link> */}
+
+            {/* { results && <Results />} */}
+
+        </Container>
     );
 }
 
